e485f6c72b6d66d3cbaab1da0cbaaa9ca8c5ca50
unknown <juangj1b@gmail.com>
2020-12-25 23:36:23 +0100

    make it better

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ client.on("message", function(message) {
   			.setThumbnail("https://empireislands.es/wp-content/uploads/revslider/empire/Empirelog4opng.png")
   			.setDescription("Para invitar a alguien tienes que acceder al siguiente enlace:")
   			.addField("https://discord.me/empireislandsnet", 'Al entrar, dale a la pestaña "Join Server", te redireccionará a otra página web en la que confirmas que no eres un robot y tendrás tu enlace para invitar a quien quieras.')
-        .setTimestamp();
+        	.setTimestamp();
   			message.channel.send(Embed2);
   		break;
   		case "servidor":
@@ -47,9 +47,10 @@ client.on("message", function(message) {
   			.setTitle("Empire Islands")
   			.setColor(0xFF8000)
   			.setThumbnail("https://empireislands.es/wp-content/uploads/revslider/empire/Empirelog4opng.png")
-  			.setDescription("Actualizado del día 08/12/2020:")
+  			.setDescription("Para buscar el pack, tienes que buscar \"Empire Islands\" en el launcher de Technic\nPuedes descargar el launcher con ei!launcher\nActualizado del día 25/12/2020:")
   			.addField("Skyblock", 'Pack de SkyFactory 4 con un par de mods adiccionales.\nVersión: 1.12.2\n Nº de mods: 221 \nIP: sky.empireislands.es')
-        .setTimestamp();
+  			.addField("Proximamente", 'Pack de ¿?.\nVersión: 1.12.2\n Nº de mods: 221 \nIP: sky.empireislands.es')
+        	.setTimestamp();
   			message.channel.send(Embed3);
   		break;
       case "tienda":
